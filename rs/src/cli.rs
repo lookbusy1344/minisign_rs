fbbeb360f3a5fd79b8918c70bf645c70e655bc2d
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2026-02-17 13:45:45 +0000

    perf(cli): use into_owned instead of to_string on Cow<str> in default_signature_path

--- rs/src/cli.rs
+++ rs/src/cli.rs
@@ -231,7 +231,7 @@ impl Cli {
             .file_name()
             .ok_or_else(|| Error::InvalidPath(message_file.to_path_buf()))?;
 
-        let mut file_name_string = file_name.to_string_lossy().to_string();
+        let mut file_name_string = file_name.to_string_lossy().into_owned();
         file_name_string.push_str(".minisig");
         sig_path.set_file_name(file_name_string);
         Ok(sig_path)
